793edeafe0186d1fc5e3d429ef2779bdf1501afc
muhammadirfan92 <irfanabbas034@gmail.com>
2021-08-09 22:20:30 +0500

    added csv function to scrapper

--- element_scrapper_selenium.py
+++ element_scrapper_selenium.py
@@ -13,18 +13,23 @@ def scrapper(url , chromedriverpath,finder ):
     data = driver.find_element_by_id(finder)
     return data.text
 
+def store_in_csv(data):
+    import csv
+    text_lis = data.split("\n")
+    with open("data.csv", 'w', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(text_lis)
+
+
 
 if __name__ == "__main__":
-    import csv
+
     chromedriverpath = "C:\Program Files (x86)\chromedriver.exe"
     url =  "https://finance.yahoo.com/quote/%5EDJI?p=%5EDJI"
     finder="Lead-4-QuoteHeader-Proxy"
     data = scrapper(url, chromedriverpath, finder)
-    text_lis = data.split("\n")
-    with open("data.csv",'w',newline="") as f:
-        writer = csv.writer(f)
-        for i in text_lis:
-            writer.writerow(i)
+    store_in_csv(data)
+
 
 
 
